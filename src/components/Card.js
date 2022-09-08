51978c26aa1ed1b23dc61e238a15bd7a70f97fcf
Julien Jamet <julien.jamet.34@gmail.com>
2022-09-08 15:40:22 +0200

    range

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Card = (props) => {
-    console.log(props);
+const Card = ({ country }) => {
     return (
         <li className="card">
-
+            <img src={country.flags.svg} alt={"drapeau" + ' ' + country.translations.fra.common} />
             <div className="infos">
-                <h2>Nom du pays</h2>
+                <h2>{country.translations.fra.common}</h2>
+                <h4>{country.capital}</h4>
+                <p>Pop: {country.population.toLocaleString()}</p>
             </div>
         </li>
     );
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,7 @@ import Card from "./Card";
 
 const Countries = () => {
     const [data, setData] = useState([])
+    const [rangeValue, setRangeValue] = useState(36)
 
     useEffect(() => {
         axios
@@ -13,10 +14,13 @@ const Countries = () => {
 
     return (
         <div className="countries">
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue} onChange={(e) => setRangeValue(e.target.value)} />
+            </ul>
             <ul>
                 {
-                    data.map((country, index) =>
-                        <Card key={index} />
+                    data.slice(0, rangeValue).map((country, index) =>
+                        <Card key={index} country={country} />
                     )
                 }
             </ul>
