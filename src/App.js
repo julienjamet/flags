38e00cbf7b85b9f8926fefe08e4dc01295396451
Julien JAMET <julienjamet1992@protonmail.com>
2025-08-05 17:14:36 +0200

    clean & debug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-// https://www.youtube.com/watch?v=f0X1Tl8aHtA
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+      <Home />
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
-import React from "react";
 import Logo from "../components/Logo";
-import Navigation from "../components/Navigation";
 import Countries from "../components/Countries";
 
 const Home = () => {
     return (
         <div>
             <Logo />
-            <Navigation />
+
             <Countries />
         </div>
     );
